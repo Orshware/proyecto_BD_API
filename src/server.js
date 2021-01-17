d9b0c25bbff55914546a7d3016e10556e5da9d9b
reitmas32 <rafa.zamora.rals@gmail.com>
2021-01-16 22:17:35 -0600

    Se añadio otra ruta al server

--- src/server.js
+++ src/server.js
@@ -7,11 +7,15 @@ const apiRoute = ''
 //Routes
 import root from './routes/index.routes'
 
+import cliente from './routes/cliente.routes'
+
 //Setings
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.json());
 
 app.use(apiRoute ,root);
+
+app.use(apiRoute + '/cliente', cliente);
 
 export default app;
