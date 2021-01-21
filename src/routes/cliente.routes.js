96dc7109485c706fb5fefd38c1f792aa04921c42
reitmas32 <rafa.zamora.rals@gmail.com>
2021-01-21 05:30:40 -0600

    Se añadio una ruta para insertar juntos un Cliente y un Domicilio

--- src/routes/cliente.routes.js
+++ src/routes/cliente.routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { Cliente } from '../models/cliente';
+import { Domicilio } from '../models/domicilio';
+import { Domicilio_Cliente } from '../models/domicilio_cliente';
 import data_base from '../database';
 
 const router = Router();
@@ -25,6 +27,44 @@ router.post('/', async (req, res) => {
     res.json(response);
 });
 
+router.post('/domicilio', async (req, res) => {
+
+    const cliente = new Cliente(
+        null, 
+        req.body.nombre, 
+        req.body.ap_pat, 
+        req.body.ap_mat, 
+        req.body.razon_social, 
+        req.body.email
+    );
+
+    const domicilio = new Domicilio(
+        null, 
+        req.body.estado, 
+        req.body.colonia, 
+        req.body.calle, 
+        req.body.numero, 
+        req.body.cp
+    );
+    const response_cliente = await data_base.query('INSERT INTO CLIENTE(nombre, ap_pat, ap_mat, razon_social, email) VALUES ($1, $2, $3, $4, $5);', [cliente.nombre, cliente.ap_pat, cliente.ap_mat, cliente.razon_social, cliente.email]);
+
+    const response_domicilio = await data_base.query('INSERT INTO DOMICILIO(estado, colonia, calle, numero, cp) VALUES ($1, $2, $3, $4, $5);', [domicilio.estado, domicilio.colonia, domicilio.calle, domicilio.numero, domicilio.cp]);
+
+    const response_id_cliente = await data_base.query('SELECT id_Cliente FROM CLIENTE WHERE nombre = $1 AND ap_pat = $2 AND ap_mat = $3 AND email = $4;', [cliente.nombre, cliente.ap_pat, cliente.ap_mat, cliente.email]);
+
+    const response_id_domicilio = await data_base.query('SELECT id_Domicilio FROM DOMICILIO WHERE estado = $1 AND colonia = $2 AND calle = $3 AND numero = $4 AND cp = $5;', [domicilio.estado, domicilio.colonia, domicilio.calle, domicilio.numero, domicilio.cp]);
+    
+    const domicilio_cliente = new Domicilio_Cliente(
+        null,
+        response_id_cliente.rows[0].id_cliente,
+        response_id_domicilio.rows[0].id_domicilio
+    );
+
+    const response_domicilio_cliente = await data_base.query('INSERT INTO DOMICILIO_CLIENTE(id_cliente, id_domicilio) VALUES($1, $2);', [domicilio_cliente.id_cliente, domicilio_cliente.id_domicilio]);
+    
+    res.json(domicilio_cliente.toJson());
+});
+
 router.patch('/', async (req, res) => {
 
     const cliente = new Cliente(
@@ -37,8 +77,6 @@ router.patch('/', async (req, res) => {
     );
 
     const response = await data_base.query('SELECT id_Cliente FROM CLIENTE WHERE nombre = $1 AND ap_pat = $2 AND ap_mat = $3 AND email = $4;', [cliente.nombre, cliente.ap_pat, cliente.ap_mat, cliente.email]);
-
-    //response.body = domicilio.toJson();
 
     res.json(response.rows);
 });
