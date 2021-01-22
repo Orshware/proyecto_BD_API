9cf4399587f8f7d30257f35b1cafa4c38f279205
reitmas32 <rafa.zamora.rals@gmail.com>
2021-01-22 15:13:45 -0600

    Se creo un metodo que crea una venta vacia

--- src/routes/venta.routes.js
+++ src/routes/venta.routes.js
@@ -20,9 +20,26 @@ router.post('/', async (req, res) => {
         req.body.fecha_venta
     );
 
-    const response = await data_base.query('INSERT INTO VENTA(id_cliente, cant_art_total, precio_total, fecha_venta) VALUES ($1, $2, $3);', [venta.id_cliente, venta.cant_art_total, venta.precio_total, venta.fecha_venta]);
+    const response = await data_base.query('INSERT INTO VENTA(id_cliente, cant_art_total, precio_total, fecha_venta) VALUES ($1, $2, $3, $4);', [venta.id_cliente, venta.cant_art_total, venta.precio_total, venta.fecha_venta]);
 
     res.json(response);
+});
+
+router.post('/new', async (req, res) => {
+
+    const venta = new Venta(
+        null, 
+        req.body.id_cliente, 
+        0, 
+        0, 
+        '2021-01-21'
+    );
+
+    const response = await data_base.query('INSERT INTO VENTA(id_cliente, cant_art_total, precio_total, fecha_venta) VALUES ($1, $2, $3, $4);', [venta.id_cliente, venta.cant_art_total, venta.precio_total, venta.fecha_venta]);
+
+    const response_id_venta = await data_base.query('SELECT id_venta FROM venta ORDER BY id_venta DESC LIMIT 1'); 
+
+    res.json(response_id_venta.rows);
 });
 
 router.patch('/', async (req, res) => {
