2caa23b0592821869d13afe2adce8532fcc1226d
reitmas32 <rafa.zamora.rals@gmail.com>
2021-01-22 18:53:20 -0600

    Se creo la conneccion a una base de datos remota

--- src/database.js
+++ src/database.js
@@ -1,11 +1,12 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'papeleria',
-    database: 'papeleria_system_test',
+    host: 'ec2-54-156-85-145.compute-1.amazonaws.com',
+    user: 'fmvxyqlwzcrifv',
+    database: 'd6gndvk95jok6t',
     port: '5432',
-    password: 'papeleria1234'
+    password: '75cbbbb7f7746b7af735919fddef1d9467455f3356f1c5807511ac25d3475e98',
+    ssl: { rejectUnauthorized: false }
 });
 
 export default pool;
